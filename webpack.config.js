9c31d2585e573891c9fa20b7c4e77ec29f8bdc92
Viet Ky <kq.viet@gmail.com>
2016-02-19 18:32:55 +0700

    Remove external deps in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 var path = require('path');
 
-module.exports = {
+var deps = [
+    'jquery.min.js',
+    'bootstrap.min.js',
+    'react-0.14.7.min.js',
+    'react-dom-0.14.7.min.js',
+    'react-bootstrap.min.js'
+];
+
+var config = {
     entry: [
         'webpack/hot/dev-server',
         'webpack-dev-server/client?http://localhost:8080',
@@ -10,7 +18,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    resolve: {
+        alias: {}
+    },
+    externals: {
+        'jquery': 'jQuery',
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'react-bootstrap': 'react-bootstrap',
+    },
     module: {
+        noParse: [],
         loaders: [
             {
                 test: /\.(js)$/,
@@ -20,3 +38,13 @@ module.exports = {
         ],
     }
 };
+
+var libPath = path.join(__dirname, '/app/libs/');
+for (var dep in deps) {
+    var depPath = path.resolve(libPath, dep);
+    //    config.resolve.alias[]
+    config.module.noParse.push(depPath);
+    console.log(depPath);
+}
+
+module.exports = config;
